41a2ffb9090e600f5b343c9ee483b41f3df3c752
Muritala Tobi <muritala.mt@gmail.com>
2020-05-24 18:23:08 +0000

    Sunday commit

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -28,5 +28,7 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         Passport::routes();
+
+        Passport::loadKeysFrom('/secret-keys/oauth');
     }
 }
